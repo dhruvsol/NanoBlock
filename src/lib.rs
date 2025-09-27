2e82351ce4212df98112c2cdb90968627f236212
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-09-27 17:26:39 +0530

    allowed strctures

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+#![no_main]
+
+use aya::maps::array;
+use aya_ebpf::{bindings::xdp_action, programs::XdpContext};
+use core::net::IpAddr;
+
+#[derive(Default)]
+#[repr(C)]
+pub struct IpConfig {
+    pub port: Option<u16>,
+    pub protocol: Option<u8>,
+    pub allowed: bool,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[array]
+static mut ALLOWED_PORTS: HashMap<u32, u32> = HashMap::<u32, u32>::with_max_entries(1024, 0); // allow ports for everyone without no ip block
+#[array]
+static mut ALLOWED_IPS: HashMap<u32, u32> = HashMap::<u32, u32>::with_max_entries(1024, 0); // allowed ips for everyone without no port block
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[map]
+static mut ALLOWED_IP_CONFIG: HashMap<u32, IpConfig> =
+    HashMap::<IpAddr, IpConfig>::with_max_entries(1024, 0);
+
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
     }
+
+    Ok((start + offset) as *const T)
+}
+
+pub fn check_packet(ctx: XdpContext) -> Result<bool, ()> {
+    return Ok(true);
 }
